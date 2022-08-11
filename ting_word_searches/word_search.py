1e8f30f55c03837f7773fb95d007dede353a2fa9
ZekaEu <zeka.surf@hotmail.com>
2022-08-11 05:36:20 -0300

    Requisito 6?

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    file = instance.data[0]
+    occurrence_list = []
+    for i, file_row in enumerate(file["linhas_do_arquivo"]):
+        if word.lower() in file_row.lower():
+            occurrence_list.append({"linha": i + 1})
+    if occurrence_list:
+        return [{
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": occurrence_list,
+        }]
+    else:
+        return occurrence_list
 
 
 def search_by_word(word, instance):
